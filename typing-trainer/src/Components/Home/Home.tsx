aa31ac3a6db13bcd2c78012939c4a3a67a974f42
Tom Reece <tomreece@toms-mbp.home>
2022-08-08 12:36:57 +0100

    stop rendering UserParagraph as it's own component for now to remove complexity

--- typing-trainer/src/Components/Home/Home.tsx
+++ typing-trainer/src/Components/Home/Home.tsx
@@ -1,23 +1,59 @@
-import { useState } from "react"
+import { SetStateAction, JSXElementConstructor, ReactElement, ReactFragment, useState } from "react"
 import "./Home.scss"
 
 import TargetParagraph from "./TargetParagraph/TargetParagraph"
-import UserParagraph from "./UserParagraph/UserParagraph"
+// import UserParagraph from "./UserParagraph/UserParagraph"
 
-
+import TextField from '@mui/material/TextField';
 
 
 
 
 export default function Home() {
 
+
+
     const [userInput, setUserInput] = useState('');
+    const [activeWordIndex, setActiveWordIndex] = useState(0);
+
+    const cloud = 'apple banana carrot'.split(' ');
+
+    function processInput(value: string) {
+        if(value.endsWith(' ')) {
+            setActiveWordIndex(index => index +1)
+            setUserInput('')
+        } else {
+            setUserInput(value)
+        }
+        }
 
     return (
         <div className="home">
 
             <TargetParagraph />
-            <UserParagraph userInput={userInput} setUserInput={setUserInput}/>
+            <p>{cloud.join(' ')}</p>
+            <p>{cloud.map((word: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | null | undefined, index: any) => {
+                if (index === activeWordIndex) {
+                    return <b>{word} </b>
+                }
+                return <span>{word} </span>
+            })}</p>
+            <p>{userInput}</p>
+
+            <TextField type="text" value={userInput} 
+    onChange={(e)=>{
+        // props.setUserInput(e.target.value)
+        processInput(e.target.value);
+    }}
+sx={{ borderTop: 1, borderBottom: 1, borderRadius: '16px', mt: "21rem", mb: "15rem", width: 450, bgcolor: 'white', color: "white", input: { color: 'black' }}} id="filled-basic" label="Type here" variant="filled" InputLabelProps={{
+    style: { color: 'black' },
+  }}/>
+
+            {/* <UserParagraph
+            userInput={userInput} setUserInput={setUserInput}
+            activeWordIndex={activeWordIndex} setActiveWordIndex={setActiveWordIndex}
+            processInput={processInput}
+            /> */}
         </div>
     )
 }
--- typing-trainer/src/Components/Home/UserParagraph/UserParagraph.tsx
+++ typing-trainer/src/Components/Home/UserParagraph/UserParagraph.tsx
@@ -5,20 +5,27 @@ import TextField from '@mui/material/TextField';
 import { FC } from "react";
 
 
+
+
 interface UserParagraphProps {
     userInput?: string;
     setUserInput?: any;
+    processInput?: any;
 }
 
 
 const UserParagraph:FC<UserParagraphProps> = (props) => {
 console.log("userInput is " + props.userInput);
 
+
+
     return (
 <div>
+    
 <TextField type="text" value={props.userInput} 
     onChange={(e)=>{
-        props.setUserInput(e.target.value)
+        // props.setUserInput(e.target.value)
+        props.processInput(e.target.value);
     }}
 sx={{ borderTop: 1, borderBottom: 1, borderRadius: '16px', mt: "21rem", mb: "15rem", width: 450, bgcolor: 'white', color: "white", input: { color: 'black' }}} id="filled-basic" label="Type here" variant="filled" InputLabelProps={{
     style: { color: 'black' },
