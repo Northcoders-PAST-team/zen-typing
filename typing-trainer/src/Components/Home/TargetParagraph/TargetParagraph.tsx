ce5b0d91a9ded4f979094db64cb8ad4323cf88ce
Tom Reece <tomreece@toms-mbp.home>
2022-08-08 10:46:42 +0100

    test commit change sample paragraph

--- typing-trainer/src/Components/Home/TargetParagraph/TargetParagraph.tsx
+++ typing-trainer/src/Components/Home/TargetParagraph/TargetParagraph.tsx
@@ -15,7 +15,7 @@ export default function TargetParagraph() {
         <Fragment>
       <CssBaseline />
       <Container maxWidth="sm" >
-        <Box sx={{ borderLeft: 1, borderRight: 1, borderRadius: '16px', mt: "1.5rem", bgcolor: '#black', height: '20vh', color: 'white',  fontFamily: 'Georgia', }} >     <p>A sample paragraph for users to match by typing</p></Box>
+        <Box sx={{ borderLeft: 1, borderRight: 1, borderRadius: '16px', mt: "1.5rem", bgcolor: '#black', height: '20vh', color: 'white',  fontFamily: 'Georgia', }} >     <p>A sample paragraph for users to match by typing!</p></Box>
       </Container>
     </Fragment>)
 }
