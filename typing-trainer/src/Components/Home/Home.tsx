dd125e7c3478f7070a6c762689f17dc4f5401cf1
Tom Reece <tomreece@toms-mbp.home>
2022-08-08 14:44:07 +0100

    add basic exercise functionality

--- typing-trainer/src/Components/Home/Home.tsx
+++ typing-trainer/src/Components/Home/Home.tsx
@@ -1,13 +1,41 @@
-import { SetStateAction, JSXElementConstructor, ReactElement, ReactFragment, useState } from "react"
+import { useState} from "react"
 import "./Home.scss"
+import { FC } from "react";
+import "../Home/TargetParagraph/TargetParagraph"
+
+import { Fragment } from "react";
+
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 import TargetParagraph from "./TargetParagraph/TargetParagraph"
 // import UserParagraph from "./UserParagraph/UserParagraph"
 
 import TextField from '@mui/material/TextField';
+import React from "react";
 
+interface WordProps {
+    text?: string;
+    active?: boolean;
+    correct?: any;
+    incorrect?: any;
+}let Word:FC<WordProps> = (props) => {
+    const {text, active, correct} = props;
+    if (correct === true) {
+        return <span className= "correct">{text} </span>
+    }
+    if (correct === false) {
+        return <span className= "incorrect">{text} </span>
+    }
+    if (active === true) {
+        return <span className= "active">{text} </span>
+    }
+    return <span>{text} </span>
 
+}
 
+Word = React.memo(Word)
 
 export default function Home() {
 
@@ -16,33 +44,65 @@ export default function Home() {
     const [userInput, setUserInput] = useState('');
     const [activeWordIndex, setActiveWordIndex] = useState(0);
 
+    const emptyBooleanArray: boolean[] = [];
+    const [correctWordArray, setCorrectWordArray] = useState(emptyBooleanArray);
+
     const cloud = 'apple banana carrot'.split(' ');
 
     function processInput(value: string) {
         if(value.endsWith(' ')) {
+            //user has finished a word
             setActiveWordIndex(index => index +1)
-            setUserInput('')
-        } else {
-            setUserInput(value)
-        }
+            setUserInput('');
+            
+            setCorrectWordArray((data : boolean[]) => {
+                const word = value.trim()
+                const newResult: boolean[] = [...data]
+                newResult[activeWordIndex] = word === cloud[activeWordIndex]
+    
+                    return newResult
+            })
+
+            } else {
+                setUserInput(value)
+            }
+
         }
 
     return (
         <div className="home">
 
-            <TargetParagraph />
-            <p>{cloud.join(' ')}</p>
-            <p>{cloud.map((word: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | null | undefined, index: any) => {
-                if (index === activeWordIndex) {
-                    return <b>{word} </b>
-                }
-                return <span>{word} </span>
-            })}</p>
+            {/* <TargetParagraph /> */}
+            <Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm" >
+        <Box sx={{ borderLeft: 1, borderRight: 1, borderRadius: '16px', mt: "1.5rem", bgcolor: '#black', height: '20vh', color: 'white',  fontFamily: 'Georgia', }} >
+            <p>{cloud.map((word: string, index: any) => {
+
+return <Word
+text={word}
+active={index === activeWordIndex}
+correct={correctWordArray[index]}
+/>
+
+})}</p>
+            </Box>
+      </Container>
+    </Fragment>
+    {/* <p>{cloud.join(' ')}</p> */}
+            {/* <p>{cloud.map((word: string, index: any) => {
+
+                    return <Word
+                    text={word}
+                    active={index === activeWordIndex}
+                    correct={correctWordArray[index]}
+                    />
+                
+            })}</p> */}
             <p>{userInput}</p>
 
             <TextField type="text" value={userInput} 
     onChange={(e)=>{
-        // props.setUserInput(e.target.value)
         processInput(e.target.value);
     }}
 sx={{ borderTop: 1, borderBottom: 1, borderRadius: '16px', mt: "21rem", mb: "15rem", width: 450, bgcolor: 'white', color: "white", input: { color: 'black' }}} id="filled-basic" label="Type here" variant="filled" InputLabelProps={{
