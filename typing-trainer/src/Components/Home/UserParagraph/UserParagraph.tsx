0cb05550acceb30b1cd541596091df115d63961b
Tom Reece <tomreece@toms-mbp.home>
2022-08-08 11:45:48 +0100

    give UserParagraph the value of the state and an onchange to set the state

--- typing-trainer/src/Components/Home/UserParagraph/UserParagraph.tsx
+++ typing-trainer/src/Components/Home/UserParagraph/UserParagraph.tsx
@@ -7,18 +7,20 @@ import { FC } from "react";
 
 interface UserParagraphProps {
     userInput?: string;
-    setUserInput?: object;
+    setUserInput?: any;
 }
 
 
 const UserParagraph:FC<UserParagraphProps> = (props) => {
-    console.log(props);
 console.log("userInput is " + props.userInput);
-console.log("setUserInput is " + props.setUserInput);
 
     return (
 <div>
-<TextField sx={{ borderTop: 1, borderBottom: 1, borderRadius: '16px', mt: "21rem", mb: "15rem", width: 450, bgcolor: 'white', color: "white", input: { color: 'black' }}} id="filled-basic" label="Type here" variant="filled" InputLabelProps={{
+<TextField type="text" value={props.userInput} 
+    onChange={(e)=>{
+        props.setUserInput(e.target.value)
+    }}
+sx={{ borderTop: 1, borderBottom: 1, borderRadius: '16px', mt: "21rem", mb: "15rem", width: 450, bgcolor: 'white', color: "white", input: { color: 'black' }}} id="filled-basic" label="Type here" variant="filled" InputLabelProps={{
     style: { color: 'black' },
   }}/>
 </div>
