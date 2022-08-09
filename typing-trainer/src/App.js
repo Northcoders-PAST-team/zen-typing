6e5271837393af47c54922bf05e168667250f938
scottkelly36 <scottkelly36@gmail.com>
2022-08-09 13:56:18 +0100

    loading component and added routing to app.js

--- typing-trainer/src/App.js
+++ typing-trainer/src/App.js
@@ -2,12 +2,17 @@ import "./App.css";
 
 import Nav from "./Components/Nav/Nav";
 import Home from "./Components/Home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Home />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
