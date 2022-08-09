cadb8877da136293645e5f491d37ce01173725d6
scottkelly36 <scottkelly36@gmail.com>
2022-08-09 16:17:32 +0100

    started user page

--- typing-trainer/src/App.js
+++ typing-trainer/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Nav from "./Components/Nav/Nav";
 import Home from "./Components/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import User from "./Components/User/User";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Nav />
         <Routes>
           <Route path={"/"} element={<Home />} />
+          <Route path={"/users/:user_id"} element={<User />} />
         </Routes>
       </BrowserRouter>
     </div>
