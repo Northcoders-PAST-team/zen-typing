c2193ec6c10c924dc56dac552680125b0d0790b5
Tom Reece <tomreece@toms-mbp.home>
2022-08-15 12:10:28 +0100

    fix typo on exercise card difficulty

--- typing-trainer/src/Components/History/ExerciseCard.jsx
+++ typing-trainer/src/Components/History/ExerciseCard.jsx
@@ -34,7 +34,7 @@ export default function ExerciseCard(props) {
       <p>Time: {time}</p>
       <p>WPM: {wpm} </p>
       <p>Accuracy {(accuracy * 100).toFixed(2)}% </p>
-      <p>Difficulty:{difficulty}</p>
+      <p>Difficulty: {difficulty}</p>
       <p>Neutral: {neutral.toFixed(2)}%</p>
     </div>
   );
--- typing-trainer/src/Components/History/History.jsx
+++ typing-trainer/src/Components/History/History.jsx
@@ -16,40 +16,32 @@ import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function History({ auth }) {
   const [user] = useAuthState(auth);
-  console.log(user, "<<user");
   const exercisesRef = collection(db, "exercises");
-  const q = query(
-    exercisesRef,
-    where("user", "==", "tom reece"),
-    orderBy("createdAt", "desc"),
-    limit(5)
-  );
+  let q = undefined;
+  if (user) {
+    q = query(
+      exercisesRef,
+      where("user", "==", user.displayName),
+      orderBy("createdAt", "desc"),
+      limit(5)
+    );
+  } else {
+    q = query(exercisesRef, orderBy("createdAt", "desc"), limit(5));
+  }
+
   const [exercises] = useCollectionData(q, {
     idField: "id",
   });
 
-  //useEffect with user in dependency array and store exercises in state
-
   return (
     <div>
-      {console.log("render history component")}
-      {console.log(exercises)}
-      {console.log(user)}
-
       <h3>History</h3>
       <div className="history">
         {exercises &&
           user &&
-          exercises
-            // .filter((exercise) => {
-            //   return exercise.user === user.displayName;
-            // })
-            // .filter((exercise, index) => {
-            //   return index <= 4;
-            // })
-            .map((exercise, index) => {
-              return <ExerciseCard key={index} exercise={exercise} />;
-            })}
+          exercises.map((exercise, index) => {
+            return <ExerciseCard key={index} exercise={exercise} />;
+          })}
       </div>
     </div>
   );
