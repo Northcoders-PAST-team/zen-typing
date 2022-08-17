947cb9a245cbe84aabea8d3475ed38b243b2a95c
scottkelly36 <scottkelly36@gmail.com>
2022-08-17 14:38:09 +0100

    margin chnage error page

--- zen-typing/src/Components/Errors/Errors.jsx
+++ zen-typing/src/Components/Errors/Errors.jsx
@@ -7,11 +7,11 @@ const Errors = ({ errorMessage }) => {
   return !errorMessage ? (
     <>
       <SideNav />
-      <div className="error">
-        <div className="error-container">
-          <h2 className="error-code">404</h2>
-          <p className="error-message">Sorry this page could not be found</p>
-          <Link to={"/"} className="error-link">
+      <div className="errors">
+        <div className="errors-container">
+          <h2 className="errors-code">404</h2>
+          <p className="errors-message">Sorry this page could not be found</p>
+          <Link to={"/"} className="errors-link">
             Home
           </Link>
         </div>
@@ -20,11 +20,11 @@ const Errors = ({ errorMessage }) => {
   ) : (
     <>
       <SideNav />
-      <div className="error">
-        <div className="error-container">
-          <h2 className="error-code">{errorMessage.response.status}</h2>
-          <p className="error-message">{errorMessage.response.data.msg}</p>
-          <Link to={"/"} class="error-link">
+      <div className="errors">
+        <div className="errors-container">
+          <h2 className="errors-code">{errorMessage.response.status}</h2>
+          <p className="errors-message">{errorMessage.response.data.msg}</p>
+          <Link to={"/"} class="errors-link">
             Home
           </Link>
         </div>
