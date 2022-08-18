4e9390fafce0fc1755aa031bedfcbb0bb33538a4
AlistairHopson <hopson.alistair@gmail.com>
2022-08-18 09:56:25 +0100

    adjust about page paragraphs

--- zen-typing/src/Components/About/About.jsx
+++ zen-typing/src/Components/About/About.jsx
@@ -22,20 +22,20 @@ export default function About() {
       <div className="about-box">
         <p>
           Zen Typing was made by a group of Northcoders students in August 2022:
-          <br />
-          <p>
-            <a href="https://github.com/Paulos-se">Paulos Ghirmai</a>,
-            <a href="https://github.com/AlistairHopson"> Alistair Hopson</a>,
-            <a href="https://github.com/scottkelly36"> Scott Kelly</a>,{" "}
-            <a href="https://github.com/TomReece1"> Thomas Reece</a>.
-          </p>
-          <br />
-          <p>
-            {`See our project on `}
-            <a href="https://github.com/Northcoders-PAST-team/zen-typing">
-              GitHub.
-            </a>
-          </p>
+        </p>
+        <br />
+        <p>
+          <a href="https://github.com/Paulos-se">Paulos Ghirmai</a>,
+          <a href="https://github.com/AlistairHopson"> Alistair Hopson</a>,
+          <a href="https://github.com/scottkelly36"> Scott Kelly</a>,{" "}
+          <a href="https://github.com/TomReece1"> Thomas Reece</a>.
+        </p>
+        <br />
+        <p>
+          {`See our project on `}
+          <a href="https://github.com/Northcoders-PAST-team/zen-typing">
+            GitHub.
+          </a>
         </p>
       </div>
     </div>
